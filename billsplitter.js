58458b9f7b70f51d9d34378ede8ef81bdc8cbf78
Azuma Emmanuella Prosper <prosperemmanuella548@gmail.com>
2026-03-16 00:19:36 +0100

    Adding the fourth command

--- billsplitter.js
+++ billsplitter.js
@@ -6,4 +6,4 @@ let pay = totalWithTip / numberOfPeople
 
 console.log(`${"Tip Amount:"} ${tipPercent}`);
 console.log(`${"Total with tip:"} ${totalWithTip}`);
-console.log(`${"Each person pays:"} ${pay}`);
+console.log(`${"Each person pays-:"} ${pay}`);
