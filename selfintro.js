2b6d219ee16d95257753e1bc48db695c72fdfbdf
Azuma Emmanuella Prosper <prosperemmanuella548@gmail.com>
2026-03-15 02:20:24 +0100

    Added the first command

---
+++ selfintro.js
@@ -0,0 +1,9 @@
+const firstName = "Princess";
+const lastName = "Donald";
+let age = 22;
+let country = "Nigeria";
+let hobby = "Dancing";
+let selfIntro = `My name is ${firstName} ${lastName}.
+I am ${age} years old and I live in ${country}.
+In my free time I enjoy ${hobby}`;
+console.log(selfIntro);
