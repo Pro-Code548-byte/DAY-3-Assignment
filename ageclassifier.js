148f52f0a1dba9e3f09884d714a3db72b98e40b5
Azuma Emmanuella Prosper <prosperemmanuella548@gmail.com>
2026-03-16 05:51:37 +0100

    Adding the fifth command

---
+++ ageclassifier.js
@@ -0,0 +1,104 @@
+let age = 25;
+if (age <= 2) {
+  console.log("Baby");
+} else if (age <= 12) {
+  console.log("Child");
+} else if (age <= 17) {
+  console.log("Teenager");
+} else if (age <= 35) {
+  console.log("Young Adult");
+} else if (age <= 59) {
+  console.log("Adult");
+} else {
+  console.log("Senior");
+}
+
+age = 2;
+if (age <= 2) {
+  console.log("Baby");
+} else if (age <= 12) {
+  console.log("Child");
+} else if (age <= 17) {
+  console.log("Teenager");
+} else if (age <= 35) {
+  console.log("Young Adult");
+} else if (age <= 59) {
+  console.log("Adult");
+} else {
+  console.log("Senior");
+}
+
+age = 10;
+if (age <= 2) {
+  console.log("Baby");
+} else if (age <= 12) {
+  console.log("Child");
+} else if (age <= 17) {
+  console.log("Teenager");
+} else if (age <= 35) {
+  console.log("Young Adult");
+} else if (age <= 59) {
+  console.log("Adult");
+} else {
+  console.log("Senior");
+}
+
+age = 15;
+if (age <= 2) {
+  console.log("Baby");
+} else if (age <= 12) {
+  console.log("Child");
+} else if (age <= 17) {
+  console.log("Teenager");
+} else if (age <= 35) {
+  console.log("Young Adult");
+} else if (age <= 59) {
+  console.log("Adult");
+} else {
+  console.log("Senior");
+}
+
+age = 22;
+if (age <= 2) {
+  console.log("Baby");
+} else if (age <= 12) {
+  console.log("Child");
+} else if (age <= 17) {
+  console.log("Teenager");
+} else if (age <= 35) {
+  console.log("Young Adult");
+} else if (age <= 59) {
+  console.log("Adult");
+} else {
+  console.log("Senior");
+}
+
+age = 45;
+if (age <= 2) {
+  console.log("Baby");
+} else if (age <= 12) {
+  console.log("Child");
+} else if (age <= 17) {
+  console.log("Teenager");
+} else if (age <= 35) {
+  console.log("Young Adult");
+} else if (age <= 59) {
+  console.log("Adult");
+} else {
+  console.log("Senior");
+}
+
+age = 70;
+if (age <= 2) {
+  console.log("Baby");
+} else if (age <= 12) {
+  console.log("Child");
+} else if (age <= 17) {
+  console.log("Teenager");
+} else if (age <= 35) {
+  console.log("Young Adult");
+} else if (age <= 59) {
+  console.log("Adult");
+} else {
+  console.log("Senior");
+}
---
+++ numberrange.js
@@ -0,0 +1,71 @@
+let num = 55;
+
+if (num >= 1 && num <= 33) {
+  console.log("Low");
+} else if (num >= 34 && num <= 66) {
+  console.log("Medium");
+} else if (num >= 67 && num <= 100) {
+  console.log("High");
+} else {
+  console.log("Out of Range");
+}
+
+num = 10;
+
+if (num >= 1 && num <= 33) {
+  console.log("Low");
+} else if (num >= 34 && num <= 66) {
+  console.log("Medium");
+} else if (num >= 67 && num <= 100) {
+  console.log("High");
+} else {
+  console.log("Out of Range");
+}
+
+num = 50;
+
+if (num >= 1 && num <= 33) {
+  console.log("Low");
+} else if (num >= 34 && num <= 66) {
+  console.log("Medium");
+} else if (num >= 67 && num <= 100) {
+  console.log("High");
+} else {
+  console.log("Out of Range");
+}
+
+num = 85;
+
+if (num >= 1 && num <= 33) {
+  console.log("Low");
+} else if (num >= 34 && num <= 66) {
+  console.log("Medium");
+} else if (num >= 67 && num <= 100) {
+  console.log("High");
+} else {
+  console.log("Out of Range");
+}
+
+num = 0;
+
+if (num >= 1 && num <= 33) {
+  console.log("Low");
+} else if (num >= 34 && num <= 66) {
+  console.log("Medium");
+} else if (num >= 67 && num <= 100) {
+  console.log("High");
+} else {
+  console.log("Out of Range");
+}
+
+num = 100;
+
+if (num >= 1 && num <= 33) {
+  console.log("Low");
+} else if (num >= 34 && num <= 66) {
+  console.log("Medium");
+} else if (num >= 67 && num <= 100) {
+  console.log("High");
+} else {
+  console.log("Out of Range");
+}
