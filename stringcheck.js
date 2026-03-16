67e209dffd6582bb6bfdcf02dd4904ba386efc15
Azuma Emmanuella Prosper <prosperemmanuella548@gmail.com>
2026-03-16 06:56:25 +0100

    Adding the eleveth command

---
+++ stringcheck.js
@@ -0,0 +1,9 @@
+const sentence = "JavaScript is fun";
+console.log(sentence.length);
+console.log(sentence.toUpperCase());
+console.log(sentence.toLowerCase());
+if (sentence.includes("fun")) {
+  console.log(true);
+} else {
+  console.log(false);
+}
