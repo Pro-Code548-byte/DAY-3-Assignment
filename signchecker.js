97f7a7092601ebe614ec19db6b9152039f05f2ad
Azuma Emmanuella Prosper <prosperemmanuella548@gmail.com>
2026-03-16 22:07:50 +0100

    Adding the fourteenth command

---
+++ signchecker.js
@@ -0,0 +1,53 @@
+let number = -15;
+if (number > 0) {
+  console.log("Positive");
+} else if (number < 0) {
+  console.log("Negative");
+} else {
+  console.log("Zero");
+}
+
+number = -100;
+if (number > 0) {
+  console.log("Positive");
+} else if (number < 0) {
+  console.log("Negative");
+} else {
+  console.log("Zero");
+}
+
+number = 0;
+if (number > 0) {
+  console.log("Positive");
+} else if (number < 0) {
+  console.log("Negative");
+} else {
+  console.log("Zero");
+}
+
+number = 1;
+if (number > 0) {
+  console.log("Positive");
+} else if (number < 0) {
+  console.log("Negative");
+} else {
+  console.log("Zero");
+}
+
+number = -1;
+if (number > 0) {
+  console.log("Positive");
+} else if (number < 0) {
+  console.log("Negative");
+} else {
+  console.log("Zero");
+}
+
+number = 999;
+if (number > 0) {
+  console.log("Positive");
+} else if (number < 0) {
+  console.log("Negative");
+} else {
+  console.log("Zero");
+}
