35ea5fc1e317066c9a3b8bd8e9f3ba3c7e1f2b96
Azuma Emmanuella Prosper <prosperemmanuella548@gmail.com>
2026-03-16 06:47:49 +0100

    Adding the tenth command

---
+++ compare.js
@@ -0,0 +1,35 @@
+let a = 42;
+let b = 78;
+let c = 55;
+
+if (a >= b && a >= c) {
+  console.log(a);
+} else if (b >= a && b >= c) {
+  console.log(b);
+} else {
+  console.log(c);
+}
+
+a = 60;
+b = 58;
+c = 62;
+
+if (a >= b && a >= c) {
+  console.log(a);
+} else if (b >= a && b >= c) {
+  console.log(b);
+} else {
+  console.log(c);
+}
+
+a = 3;
+b = 1;
+c = 3;
+
+if (a >= b && a >= c) {
+  console.log(a);
+} else if (b >= a && b >= c) {
+  console.log(b);
+} else {
+  console.log(c);
+}
