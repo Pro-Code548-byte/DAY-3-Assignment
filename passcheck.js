d60d475d1f70d03ea191107d10d1805626145a8c
Azuma Emmanuella Prosper <prosperemmanuella548@gmail.com>
2026-03-17 05:50:51 +0100

    Adding the seventeenth command

---
+++ passcheck.js
@@ -0,0 +1,44 @@
+let password = "hello";
+if (password.length < 6) {
+  console.log("Weak");
+} else if (password.length <= 9) {
+  console.log("Moderate");
+} else {
+  console.log("Strong");
+}
+
+password = "abc";
+if (password.length < 6) {
+  console.log("Weak");
+} else if (password.length <= 9) {
+  console.log("Moderate");
+} else {
+  console.log("Strong");
+}
+
+password = "mypass";
+if (password.length < 6) {
+  console.log("Weak");
+} else if (password.length <= 9) {
+  console.log("Moderate");
+} else {
+  console.log("Strong");
+}
+
+password = "sunshine";
+if (password.length < 6) {
+  console.log("Weak");
+} else if (password.length <= 9) {
+  console.log("Moderate");
+} else {
+  console.log("Strong");
+}
+
+password = "SecurePass2024";
+if (password.length < 6) {
+  console.log("Weak");
+} else if (password.length <= 9) {
+  console.log("Moderate");
+} else {
+  console.log("Strong");
+}
